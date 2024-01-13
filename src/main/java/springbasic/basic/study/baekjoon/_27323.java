ce611a31e86e756194439df43d21fae6ed5534fd
MasJeong <jihun3654@naver.com>
2024-01-13 16:10:46 +0900

    백준 추가

---
+++ src/main/java/springbasic/basic/study/baekjoon/_27323.java
@@ -0,0 +1,19 @@
+package springbasic.basic.study.baekjoon;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _27323 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num1 = Integer.parseInt(br.readLine());
+        int num2 = Integer.parseInt(br.readLine());
+
+        System.out.print(num1 * num2);
+
+        br.close();
+    }
+}
