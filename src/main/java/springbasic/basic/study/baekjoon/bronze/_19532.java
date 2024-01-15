2e55553519fceafe5a1823f6330410b2b22a5cd5
jeongjihun <jihun3654@naver.com>
2024-01-15 12:36:15 +0900

    브루트포스 19532 추가

---
+++ src/main/java/springbasic/basic/study/baekjoon/bronze/_19532.java
@@ -0,0 +1,37 @@
+package springbasic.basic.study.baekjoon.bronze;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+// TODO x=(c*e-b*f)/(a*e-b*d)
+// y= (c*d -a*f)/(db-ae)
+public class _19532 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+
+        Loop1:
+        for(int i = -999; i <= 999; i++)
+            Loop2:
+            for(int j = -999; j <= 999; j++)
+                if((a * i + b * j == c) && (d * i + e * j == f)) {
+                    bw.write(i + " " + j);
+                    break Loop1;
+                }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+}
