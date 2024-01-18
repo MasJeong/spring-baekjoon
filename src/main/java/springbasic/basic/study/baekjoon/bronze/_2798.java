8affdbd2d7cf78c4ac9b0d7bc5bba65cd2724090
jeongjihun <jihun3654@naver.com>
2024-01-18 12:32:22 +0900

    브루트포스 2798 완료

--- src/main/java/springbasic/basic/study/baekjoon/bronze/_2798.java
+++ src/main/java/springbasic/basic/study/baekjoon/bronze/_2798.java
@@ -1,7 +1,6 @@
 package springbasic.basic.study.baekjoon.bronze;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class _2798 {
@@ -15,14 +14,31 @@ public class _2798 {
 
         st = new StringTokenizer(br.readLine());
         int[] arrCard = new int[n];
+        int max = -1;
+        int result = 0;
 
         int idx = 0;
         while(st.hasMoreTokens()) {
             arrCard[idx++] = Integer.parseInt(st.nextToken());
         }
 
+        outer:
+        for(int i = 0; i < arrCard.length -2; i++)
+            for(int j = i + 1; j < arrCard.length - 1; j++)
+                for(int k = j + 1; k < arrCard.length; k++) {
+                    result = arrCard[i] + arrCard[j] + arrCard[k];
+
+                    if(result == m) {
+                        max = result;
+                        break outer;
+                    } else if(result < m) {
+                        if(max < result)
+                            max = result;
+                    }
+                }
+
         br.close();
-//        bw.write(Integer.toString(max));
+        bw.write(Integer.toString(max));
         bw.flush();
         bw.close();
     }
