132f52dfa00701be5c4cdd53397f581d3512b76c
jeongjihun <jihun3654@naver.com>
2024-02-01 12:43:09 +0900

    브루트포스 분해합 추가

--- src/main/java/springbasic/basic/study/baekjoon/bronze/bruteforce/_2231_separateSum.java
+++ src/main/java/springbasic/basic/study/baekjoon/bronze/bruteforce/_2231_separateSum.java
@@ -3,43 +3,36 @@ package springbasic.basic.study.baekjoon.bronze.bruteforce;
 import java.io.*;
 
 /**
- * TODO 분해합
+ * 분해합 - 숫자의 한자리씩 char로 뜯어내어 비교한 방법
  * 자연수 245(n)의 분해합은 256(2+4+5)(m)
  */
 public class _2231_separateSum {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        /*
-        1,000,000 * 20까지
-        반복문은 200번까지만 돌아야한다.
-         */
-        long n = Integer.parseInt(br.readLine());
-        long m = 0;
-        char[] ch = new char[7];
+        int n = Integer.parseInt(br.readLine());
+        int num = 1;
 
-        for(int i = 1; i <= n; i++) {
-            String str = Integer.toString(i);
-            for(int j = 0; j < str.length(); j++) {
-                ch[j] = str.charAt(j);
+        while(n > num) {
+            String str = Integer.toString(num);
+            int temp = num++;
+
+            for(int i = 0 ; i < str.length(); i++) {
+                int a = str.charAt(i) - '0';
+                temp += a;
             }
 
-            m = Integer.parseInt(str);
-            for(int j = 0; j < ch.length; j++) {
-                if(ch[j] == '\u0000') break;
-
-                m += ch[j] - '0';
+            if(n == temp) {
+                break;
             }
         }
 
-        if(n == m) {
-            bw.write(Integer.toString(0));
+        if(n == num) {
+            System.out.println(0);
+        } else {
+            System.out.println(num - 1);
         }
 
         br.close();
-        bw.write(Long.toString(m));
-        bw.flush();
-        bw.close();
     }
 }
