4d752b4a7608c01582d7cb2999b5caaab21d72a1
MasJeong <jihun3654@naver.com>
2024-02-06 23:37:34 +0900

    이분탐색 - 징검다리 문제 풀이에 대해 방향성을 잡지 못하여 다시 풀어보는 것이 필요함

---
+++ src/main/java/springbasic/basic/study/baekjoon/silver/binarysearch/_11561.java
@@ -0,0 +1,53 @@
+package springbasic.basic.study.baekjoon.silver.binarysearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 징검다리 - TODO 수학 및 이분탐색
+ * 등차수열의 합 (N * (N+1)) / 2
+ */
+public class _11561 {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+
+        // 입력
+        while(t-- > 0) {
+            long n = Long.parseLong(br.readLine());
+
+            //
+            long left = 0;
+            long right = Integer.MAX_VALUE;
+            long result = 0;
+
+            while(left <= right) {
+                long mid = (left + right) / 2;
+                long sum = mid * (mid + 1) / 2; // 등차수열 합(n * (n + 1) / 2)
+
+                /*
+                    등차수열의 합은 n보다 무조건 작다
+                    등차수열의 합이 n보다 작을 때 찾은 수(mid)와 결과값(result) 중 최댓값 출력.
+                    즉, n을 넘지 않는 mid(등차수열 임의 숫자(num))값을 출력한다.
+                 */
+                if(sum <= n) {
+                    result = Math.max(mid, result);
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+
+            sb.append(result).append("\n");
+        }
+
+        System.out.println(sb);
+
+        br.close();
+    }
+
+}
