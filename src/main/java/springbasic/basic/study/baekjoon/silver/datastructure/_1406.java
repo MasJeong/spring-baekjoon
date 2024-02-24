e1b74ece2f53d21d6ca2b16c4c402a8adbfdb5d3
MasJeong <jihun3654@naver.com>
2024-02-24 17:17:28 +0900

    요세푸스 if 제거 및 소스정리

--- src/main/java/springbasic/basic/study/baekjoon/silver/datastructure/_1406.java
+++ src/main/java/springbasic/basic/study/baekjoon/silver/datastructure/_1406.java
@@ -3,77 +3,42 @@ package springbasic.basic.study.baekjoon.silver.datastructure;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.ListIterator;
 import java.util.StringTokenizer;
 
 /**
- * 에디터 - 자료구조
+ * 에디터 - 자료구조 TODO 스택으로 다시풀자
+ * 1. stack을 이용하여 push 시 pointer + 1에 값이 있으면 뒤로 1칸씩 옮긴다.
+ * - pop 시 pointer부터 size -1까지 앞으로 옮긴다.
+ * -> 시간초과.
+ * 2. LinkedList 사용
+ * - add 또는 remove 시 탐색하는 과정이 너무 길다.
+ * -> 시간초과
+ * 3. LinkedList 및 ListIterator 사용
+ * -> 통과
  */
 public class _1406 {
-
-    private static char[] stack;
-    private static int pointer = -1;
-    private static int size = 0;
-
-    private static void push(char val) {
-        // L 겁나 눌러서 pointer 음수이면 맨 앞에 1개 데이터세팅
-        if (pointer < 0) {
-            char[] newStack = new char[stack.length];
-
-            for (int i = 0; i < size; i++) {
-                newStack[i + 1] = stack[i];
-            }
-
-            stack = newStack;
-
-            pointer = -1;
-        }
-
-        stack[++pointer] = val;
-        size++;
-    }
-
-    private static int pop() {
-        if(pointer < 0) {
-            return -1;
-        }
-
-        size--;
-        stack[pointer] = '\0';
-        return stack[pointer--];
-    }
-
-    private static int size() {
-        return size;
-    }
-
-    private static int top() {
-        if(size <= 0) {
-            return -1;
-        }
-
-        return stack[size - 1];
-    }
+    private static final LinkedList<Character> linkedList= new LinkedList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        /*
-            L	커서를 왼쪽으로 한 칸 옮김 (커서가 문장의 맨 앞이면 무시됨)
-            D	커서를 오른쪽으로 한 칸 옮김 (커서가 문장의 맨 뒤이면 무시됨)
-            B	커서 왼쪽에 있는 문자를 삭제함 (커서가 문장의 맨 앞이면 무시됨) - 이부분 유의하자.
-            삭제로 인해 커서는 한 칸 왼쪽으로 이동한 것처럼 나타나지만, 실제로 커서의 오른쪽에 있던 문자는 그대로임
-            P $	$라는 문자를 커서 왼쪽에 추가함
-         */
         String n = br.readLine();
         int m = Integer.parseInt(br.readLine());
-        stack = new char[n.length() + m];
         StringTokenizer st;
 
         // 처음 입력받은 문자열 push
         for (int i = 0; i < n.length(); i++) {
-            char ch = n.charAt(i);
-            push(ch);
+            linkedList.add(n.charAt(i));
+        }
+
+        // ListIterator 사용. 좌우 인덱스를 체크할 수 있음.
+        ListIterator<Character> listIterator = linkedList.listIterator();
+
+        while (listIterator.hasNext()) {
+            listIterator.next();
         }
 
         while (m-- > 0) {
@@ -82,37 +47,29 @@ public class _1406 {
 
             switch (cmd) {
                 case 'L':
-                    if (pointer < 0) break;
-
-                    // Todo L: 왼쪽 공백푸쉬, R: 오른쪽 공백푸쉬로 진행해보자 
-                    // 요솟수가 존재한다면 우측으로 1칸씩 옮겨준다.
-//                    if(stack[pointer] != '\0') {
-//                        for (int i = pointer; i < size; i++) {
-//                            char temp = stack[i + 1];
-//                            stack[i + 1] = stack[i];
-//                            stack[i] = temp;
-//                        }
-//                    }
-
-                    pointer--;
+                    if (listIterator.hasPrevious()) {
+                        listIterator.previous();
+                    }
                     break;
                 case 'D':
-                    pointer++;
+                    if (listIterator.hasNext()) {
+                        listIterator.next();
+                    }
                     break;
                 case 'B':
-                    if (pointer < 0) break;
-
-                    pop();
+                    if (listIterator.hasPrevious()) {
+                        listIterator.previous();
+                        listIterator.remove();
+                    }
                     break;
                 case 'P':
-                    push(st.nextToken().charAt(0));
+                    listIterator.add(st.nextToken().charAt(0));
                     break;
             }
         }
 
-        for (int i = 0; i < size(); i++) {
-            sb.append(stack[i]);
-        }
+        linkedList.forEach(sb::append);
+
         System.out.println(sb);
         br.close();
     }
