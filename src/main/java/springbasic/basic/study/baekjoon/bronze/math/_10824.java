86a247446f18e6961822891d68bb0d8dccab46fc
MasJeong <jihun3654@naver.com>
2024-03-05 16:44:51 +0900

    네 수

---
+++ src/main/java/springbasic/basic/study/baekjoon/bronze/math/_10824.java
@@ -0,0 +1,26 @@
+package springbasic.basic.study.baekjoon.bronze.math;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 네 수
+ */
+public class _10824 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String a = st.nextToken();
+        String b = st.nextToken();
+        String c = st.nextToken();
+        String d = st.nextToken();
+        String ab = a + b;
+        String cd = c + d;
+
+        System.out.println(Long.parseLong(ab) + Long.parseLong(cd));
+
+        br.close();
+    }
+}
